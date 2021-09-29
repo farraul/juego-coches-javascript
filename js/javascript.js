29eb9193067fe12d35e4c4b378000f340193903f
farraul <raulfarllobell@gmail.com>
2021-09-29 16:29:40 +0200

    creación de archivos

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+class Tarea {
+constructor(texto, id) {
+console.log('tarea instanciada! con el texto:', texto);
+this.texto = texto;
+this.id = id;
+this.fecha = new Date();
+}
+
+
+
+}
